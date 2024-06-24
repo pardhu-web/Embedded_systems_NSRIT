d6de5ffd5cb11df75dbf20e79c148fc2e79af15c
pardhu-web <127423774+pardhu-web@users.noreply.github.com>
2024-06-24 10:56:43 +0530

    Add files via upload

--- 8051 codes/10.motor driver interfacing/main.c
+++ 8051 codes/10.motor driver interfacing/main.c
@@ -1,10 +1,9 @@
 #include<reg51.h>
+sbit b1 = P1^0;
+sbit b2 = P1^1;
 
-sbit in1 = P2^0;
-sbit in2 = P2^1;
-sbit in3 = P2^2;
-sbit in4 = P2^3;
-
+int val = 1;
+int val2 = 128;
 void delay(int time)
 {
 	unsigned int i,j;
@@ -16,26 +15,21 @@ void delay(int time)
 
 void main()
 {
-	while(1)
-	{
-	in1 = 1; in2 = 0;
-	in3 = 1; in4 = 0;
-	delay(500);
+ while(1)
+ {
+ while(b1 == 0)
+ {
+  P2 = val;
+  delay(50);
+  val <<= 1;
+ }
 
-	in1 = 0; in2 = 1;
-	in3 = 0; in4 = 1;
-	delay(500);
-
-	in1 = 1; in2 = 0;
-	in3 = 0; in4 = 1;
-	delay(500);
-
-	in1 = 0; in2 = 1;
-	in3 = 1; in4 = 0;
-	delay(500);
-
-    in1 = 0; in2 = 0;
-	in3 = 0; in4 = 0;
-	delay(500);
-	}
+  while(b2 == 0)
+ {
+   P2 = val2;
+  delay(50);
+  val >>= 1;
+ }
+ }
+	
 }
